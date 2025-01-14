fce7aa25efd167583975f5bfe669df93d4b7d61a
Vahid Pourreza <vahid.pourreza@outlook.com>
2025-01-14 20:56:25 +0330

    add entity and aggregateroot base classes

---
+++ Onion/src/2.Core/Framework.Core.Domain/Entities/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿using Framework.Core.Domain.Events;
+
+namespace Framework.Core.Domain.Entities;
+
+
+public interface IAggregateRoot
+{
+    void ClearEvents();
+    IEnumerable<IDomainEvent> GetEvents();
+}
---
+++ Onion/src/2.Core/Framework.Core.Domain/Entities/IAuditableEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Framework.Core.Domain.Entities;
+
+public interface IAuditableEntity
+{
+
+}
