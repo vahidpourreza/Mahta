7fc4a47395803f13d5c62cb6d097026c140aba4f
Vahid <va6hid@outlook.com>
2025-01-15 10:35:01 +0330

    add extensions folder in utilities

---
+++ Onion/src/1.Utilities/Mahta.Utilities/Extensions/LinqExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Data;
+using System.Linq.Expressions;
+
+namespace Mahta.Utilities.Extensions;
+
+/// <summary>
+/// Example:
+/// var query = dbContext.Employees.AsQueryable();
+///var sortedAndFilteredQuery = query
+///    .OrderByField("Salary", true) // Sort by Salary in ascending order
+///    .WhereIf(isActive, e => e.IsActive) // Filter by IsActive condition
+///    .WhereIf(filterByDepartment, e => e.Department == "IT"); // Filter by Department condition
+/// </summary>
+public static class LinqExtensions
+{
+    public static IQueryable<T> OrderByField<T>(this IQueryable<T> q, string SortField, bool Ascending)
+    {
+        var param = Expression.Parameter(typeof(T), "p");
+        var prop = Expression.Property(param, SortField);
+        var exp = Expression.Lambda(prop, param);
+        string method = Ascending ? "OrderBy" : "OrderByDescending";
+        Type[] types = new Type[] { q.ElementType, exp.Body.Type };
+        var mce = Expression.Call(typeof(Queryable), method, types, q.Expression, exp);
+        return q.Provider.CreateQuery<T>(mce);
+    }
+
+
+    /// <summary>
+    /// Filters a <see cref="IQueryable{T}"/> by given predicate if given condition is true.
+    /// </summary>
+    /// <param name="query">Queryable to apply filtering</param>
+    /// <param name="condition">A boolean value</param>
+    /// <param name="predicate">Predicate to filter the query</param>
+    /// <returns>Filtered or not filtered query based on <paramref name="condition"/></returns>
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate)
+    {
+        return condition
+        ? query.Where(predicate)
+        : query;
+    }
+
+    /// <summary>
+    /// Filters a <see cref="IQueryable{T}"/> by given predicate if given condition is true.
+    /// </summary>
+    /// <param name="query">Queryable to apply filtering</param>
+    /// <param name="condition">A boolean value</param>
+    /// <param name="predicate">Predicate to filter the query</param>
+    /// <returns>Filtered or not filtered query based on <paramref name="condition"/></returns>
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, int, bool>> predicate)
+    {
+        return condition
+        ? query.Where(predicate)
+        : query;
+    }
+}
---
+++ Onion/src/1.Utilities/Mahta.Utilities/Extensions/ObjectExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+
+namespace Mahta.Utilities.Extensions;
+
+
+
+/// <summary>
+/// Example : 
+///var filter = new
+///{
+///    Name = "API Test",
+///    Status = "Active",
+///    Tags = new[] { "Tag1", "Tag2" }
+///};
+///string baseUrl = "https://api.example.com/resources";
+///string queryString = filter.ToQueryString();
+
+///string fullUrl = $"{baseUrl}?{queryString}";
+/// Example Result: "https://api.example.com/resources?Name=API%20Test&Status=Active&Tags=Tag1&Tags=Tag2"
+
+/// </summary>
+public static class ObjectExtensions
+{
+    public static string ToQueryString(this object obj)
+    {
+        if (obj is null) throw new ArgumentNullException("Object");
+
+        var properties = obj.GetType().GetProperties()
+            .Where(x => x.CanWrite)
+            .Where(x => x.GetValue(obj, null) is not null)
+            .Select(x => KeyValuePair.Create(x.Name, x.GetValue(obj, null))).ToList();
+
+        var propertyNames = properties
+            .Where(x => x.Value is not string && x.Value is IEnumerable)
+            .Select(x => x.Key)
+            .ToList();
+
+        foreach (var key in propertyNames)
+        {
+            var valueType = properties.FirstOrDefault(x => x.Key == key).Value.GetType();
+
+            var valueElemType = valueType.IsGenericType
+                ? valueType.GetGenericArguments()[0]
+                : valueType.GetElementType();
+
+            if (valueElemType.IsPrimitive || valueElemType == typeof(string) || valueElemType == typeof(Guid))
+            {
+                var enumerable = properties.FirstOrDefault(c => c.Key == key).Value as IEnumerable;
+
+                properties.RemoveAll(x => x.Key == key);
+
+                foreach (var item in enumerable)
+                {
+                    properties.Add(KeyValuePair.Create(key, item));
+                }
+            }
+        }
+
+        return string.Join("&", properties.Where(x => x.Value is not null)
+            .Select(x => string.Concat(
+                Uri.EscapeDataString(x.Key), "=",
+                Uri.EscapeDataString(x.Value.ToString()))));
+    }
+}
