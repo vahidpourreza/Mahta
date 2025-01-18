ca05183023ed418d9bfb1ae6a518760c6dab7a49
Vahid <va6hid@outlook.com>
2025-01-18 12:45:10 +0330

    add base command/query db context and repository

---
+++ Onion/src/3.Infra/Data/Mahta.Infra.Data.Sql.Queries/BaseQueryRepository.cs
@@ -0,0 +1,12 @@
+﻿using Mahta.Core.Contracts.Data.Queries;
+
+namespace Mahta.Infra.Data.Sql.Queries;
+
+public class BaseQueryRepository<TDbContext> : IQueryRepository  where TDbContext : BaseQueryDbContext
+{
+    protected readonly TDbContext _dbContext;
+    public BaseQueryRepository(TDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+}
