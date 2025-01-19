50b330a03c7b7a9ff7684bc2cf9e7ebf7040565d
Vahid <va6hid@outlook.com>
2025-01-19 16:52:46 +0330

    Complete the InMemoryCache service

---
+++ Extentsions/Caching/Mahta.Extensions.Caching.InMemory/Mahta.Extensions.Caching.InMemory/Extensions/DependencyInjection/InMemoryCachingServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Mahta.Extensions.Caching.Abstractions;
+using Mahta.Extensions.Caching.InMemory.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mahta.Extensions.DependencyInjection;
+
+public static class InMemoryCachingServiceCollectionExtensions
+{
+    public static IServiceCollection AddMahtaInMemoryCaching(this IServiceCollection services)
+                     => services
+                      .AddMemoryCache()
+                      .AddTransient<ICacheAdapter, InMemoryCacheAdapter>();
+}
