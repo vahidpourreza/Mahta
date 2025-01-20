5027ad01243b143aa30589dde76d47020505c53f
Vahid <va6hid@outlook.com>
2025-01-20 13:39:06 +0330

    implement Mahta.Extensions.UsersManagement

---
+++ Extentsions/UsersManagement/Mahta.Extensions.UsersManagement/Mahta.Extensions.UsersManagement/Extensions/DependencyInjection/UserInfoServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Mahta.Extensions.UsersManagement.Abstractions;
+using Mahta.Extensions.UsersManagement.Options;
+using Mahta.Extensions.UsersManagement.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mahta.Extensions.DependencyInjection;
+
+public static class UserInfoServiceCollectionExtensions
+{
+    public static IServiceCollection AddMahtaWebUserInfoService(this IServiceCollection services, IConfiguration configuration, bool useFake = false)
+    {
+        if (useFake)
+        {
+            services.AddSingleton<IUserInfoService, FakeUserInfoService>();
+
+        }
+        else
+        {
+            services.Configure<UserManagementOptions>(configuration);
+            services.AddSingleton<IUserInfoService, WebUserInfoService>();
+
+        }
+        return services;
+    }
+
+
+    public static IServiceCollection AddMahtaWebUserInfoService(this IServiceCollection services, IConfiguration configuration, string sectionName, bool useFake = false)
+    {
+        services.AddMahtaWebUserInfoService(configuration.GetSection(sectionName), useFake);
+        return services;
+    }
+
+    public static IServiceCollection AddMahtaWebUserInfoService(this IServiceCollection services, Action<UserManagementOptions> setupAction, bool useFake = false)
+    {
+        if (useFake)
+        {
+            services.AddSingleton<IUserInfoService, FakeUserInfoService>();
+
+        }
+        else
+        {
+            services.Configure(setupAction);
+            services.AddSingleton<IUserInfoService, WebUserInfoService>();
+
+        }
+        return services;
+    }
+}
+
---
+++ Extentsions/UsersManagement/Mahta.Extensions.UsersManagement/Mahta.Extensions.UsersManagement/Services/WebUserInfoService.cs
@@ -0,0 +1,44 @@
+﻿using Mahta.Extensions.UsersManagement.Abstractions;
+using Mahta.Extensions.UsersManagement.Options;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Mahta.Extensions.UsersManagement.Extensions;
+
+namespace Mahta.Extensions.UsersManagement.Services;
+
+public class WebUserInfoService(IHttpContextAccessor httpContextAccessor, IOptions<UserManagementOptions> configuration) : IUserInfoService
+{
+    private readonly UserManagementOptions _configuration = configuration.Value;
+
+    public string GetUserAgent()
+    => httpContextAccessor?.HttpContext?.Request?.Headers["User-Agent"] ?? _configuration.DefaultUserAgent;
+
+    public string GetUserIp()
+    => httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress?.ToString() ?? _configuration.DefaultUserIp;
+
+    public string UserId()
+    => httpContextAccessor?.HttpContext?.User?.GetClaim(ClaimTypes.NameIdentifier) ?? string.Empty;
+
+    public string GetUsername()
+    => httpContextAccessor.HttpContext?.User?.GetClaim(ClaimTypes.Name) ?? _configuration.DefaultUsername;
+
+    public string GetFirstName()
+    => httpContextAccessor.HttpContext?.User?.GetClaim(ClaimTypes.GivenName) ?? _configuration.DefaultFirstName;
+
+    public string GetLastName()
+    => httpContextAccessor.HttpContext?.User?.GetClaim(ClaimTypes.Surname) ?? _configuration.DefaultLastName;
+
+    public bool IsCurrentUser(string userId) => string.Equals(UserId().ToString(), userId, StringComparison.OrdinalIgnoreCase);
+
+    public string? GetClaim(string claimType)
+    => httpContextAccessor.HttpContext?.User?.GetClaim(claimType);
+
+    public string UserIdOrDefault() => UserIdOrDefault(_configuration.DefaultUserId);
+
+    public string UserIdOrDefault(string defaultValue)
+    {
+        string userId = UserId();
+        return string.IsNullOrEmpty(userId) ? defaultValue : userId;
+    }
+}
