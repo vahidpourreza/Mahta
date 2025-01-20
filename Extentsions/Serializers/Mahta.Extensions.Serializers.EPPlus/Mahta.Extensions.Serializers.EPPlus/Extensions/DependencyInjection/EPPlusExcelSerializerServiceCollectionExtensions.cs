80d495bfe1b243e8ab21f707e09c11f95d4e0fc1
Vahid <va6hid@outlook.com>
2025-01-20 12:04:26 +0330

    implement Mahta.Extensions.Serializers.EPPlus

---
+++ Extentsions/Serializers/Mahta.Extensions.Serializers.EPPlus/Mahta.Extensions.Serializers.EPPlus/Extensions/DependencyInjection/EPPlusExcelSerializerServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Mahta.Extensions.Serializers.Abstractions;
+using Mahta.Extensions.Serializers.EPPlus.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mahta.Extensions.DependencyInjection;
+
+public static class EPPlusExcelSerializerServiceCollectionExtensions
+{
+    public static IServiceCollection AddEPPlusExcelSerializer(this IServiceCollection services) => services.AddSingleton<IExcelSerializer, EPPlusExcelSerializer>();
+}
---
+++ Extentsions/Serializers/Mahta.Extensions.Serializers.EPPlus/Mahta.Extensions.Serializers.EPPlus/Services/EPPlusExcelSerializer.cs
@@ -0,0 +1,31 @@
+﻿using Mahta.Extensions.Serializers.Abstractions;
+using Mahta.Extensions.Serializers.EPPlus.Extensions;
+using Mahta.Extensions.Translations.Abstractions;
+using System.Data;
+
+namespace Mahta.Extensions.Serializers.EPPlus.Services;
+
+
+public class EPPlusExcelSerializer : IExcelSerializer
+{
+    private readonly ITranslator _translator;
+
+    #region Constructor
+    public EPPlusExcelSerializer(ITranslator translator)
+    {
+        _translator = translator;
+    }
+    #endregion
+
+
+    #region Methods
+
+    public byte[] ListToExcelByteArray<T>(List<T> list, string sheetName = "Result") => list.ToExcelByteArray(_translator, sheetName);
+
+    public DataTable ExcelToDataTable(byte[] bytes) => bytes.ToDataTableFromExcel();
+
+    public List<T> ExcelToList<T>(byte[] bytes) => bytes.ToDataTableFromExcel().ToList<T>(_translator);
+
+    #endregion
+
+}
