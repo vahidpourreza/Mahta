4d1221039d9b339fb625ea131110d0fba704f33c
Vahid <va6hid@outlook.com>
2025-01-20 11:33:52 +0330

    implement Mahta.Extensions.Serializers.Microsoft

---
+++ Extentsions/Serializers/Mahta.Extensions.Serializers.Microsoft/Mahta.Extensions.Serializers.Microsoft/Extensions/DependencyInjection/MicrosoftSerializerServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Mahta.Extensions.Serializers.Abstractions;
+using Mahta.Extensions.Serializers.Microsoft.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mahta.Extensions.DependencyInjection;
+
+public static class MicrosoftSerializerServiceCollectionExtensions
+{
+    public static IServiceCollection AddMahtaMicrosoftSerializer(this IServiceCollection services) => services.AddSingleton<IJsonSerializer, MicrosoftSerializer>();
+}
