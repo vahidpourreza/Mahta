16bd77727f8c71dd8620aa035fc813e0606b9b4c
Vahid <va6hid@outlook.com>
2025-01-20 11:42:27 +0330

    implement Mahta.Extensions.Serializers.NewtonSoft

---
+++ Extentsions/Serializers/Mahta.Extensions.Serializers.NewtonSoft/Mahta.Extensions.Serializers.NewtonSoft/Extensions/DependencyInjection/NewtonSoftSerializerServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Mahta.Extensions.Serializers.Abstractions;
+using Mahta.Extensions.Serializers.NewtonSoft.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mahta.Extensions.DependencyInjection;
+
+public static class NewtonSoftSerializerServiceCollectionExtensions
+{
+    public static IServiceCollection AddMahtaNewtonSoftSerializer(this IServiceCollection services) => services.AddSingleton<IJsonSerializer, NewtonSoftSerializer>();
+}
