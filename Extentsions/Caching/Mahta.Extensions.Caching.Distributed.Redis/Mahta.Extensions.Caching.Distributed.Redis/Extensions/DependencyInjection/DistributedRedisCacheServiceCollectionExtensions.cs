d3d365a5ae2ab7fab65de80df03cdc8f11bcaa2b
Vahid <va6hid@outlook.com>
2025-01-20 09:18:02 +0330

    impelement Mahta.Extensions.Caching.Distributed.Redis

---
+++ Extentsions/Caching/Mahta.Extensions.Caching.Distributed.Redis/Mahta.Extensions.Caching.Distributed.Redis/Extensions/DependencyInjection/DistributedRedisCacheServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Mahta.Extensions.Caching.Abstractions;
+using Mahta.Extensions.Caching.Distributed.Redis.Options;
+using Mahta.Extensions.Caching.Distributed.Redis.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mahta.Extensions.DependencyInjection;
+
+public static class DistributedRedisCacheServiceCollectionExtensions
+{
+    public static IServiceCollection AddMahtaRedisDistributedCache(this IServiceCollection services, IConfiguration configuration, string sectionName)
+        => services.AddMahtaRedisDistributedCache(configuration.GetSection(sectionName));
+
+    public static IServiceCollection AddMahtaRedisDistributedCache(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<ICacheAdapter, DistributedRedisCacheAdapter>();
+        services.Configure<DistributedRedisCacheOptions>(configuration);
+
+        var option = configuration.Get<DistributedRedisCacheOptions>();
+
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = option.Configuration;
+            options.InstanceName = option.InstanceName;
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddMahtaRedisDistributedCache(this IServiceCollection services, Action<DistributedRedisCacheOptions> setupAction)
+    {
+        services.AddTransient<ICacheAdapter, DistributedRedisCacheAdapter>();
+        services.Configure(setupAction);
+
+        var option = new DistributedRedisCacheOptions();
+        setupAction.Invoke(option);
+
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = option.Configuration;
+            options.InstanceName = option.InstanceName;
+        });
+
+        return services;
+    }
+}
---
+++ Extentsions/Caching/Mahta.Extensions.Caching.Distributed.Redis/Mahta.Extensions.Caching.Distributed.Redis/Services/DistributedRedisCacheAdapter.cs
@@ -0,0 +1,79 @@
+﻿using Mahta.Extensions.Caching.Abstractions;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using Mahta.Extensions.Serializers.Abstractions;
+
+namespace Mahta.Extensions.Caching.Distributed.Redis.Services;
+
+
+public class DistributedRedisCacheAdapter : ICacheAdapter
+{
+    private readonly IDistributedCache _cache;
+    private readonly IJsonSerializer _serializer;
+    private readonly ILogger<DistributedRedisCacheAdapter> _logger;
+
+    #region Constructor
+    public DistributedRedisCacheAdapter(IDistributedCache cache, IJsonSerializer serializer, ILogger<DistributedRedisCacheAdapter> logger)
+    {
+        _cache = cache;
+        _serializer = serializer;
+        _logger = logger;
+
+        _logger.LogInformation("DistributedCache Redis Adapter Start working");
+    }
+
+
+    #endregion
+
+    #region Methods
+
+    public void Add<TInput>(string key, TInput obj, DateTime? absoluteExpiration, TimeSpan? slidingExpiration)
+    {
+        _logger.LogTrace("DistributedCache Redis Adapter Cache {obj} with key : {key} " +
+                         ", with data : {data} " +
+                         ", with absoluteExpiration : {absoluteExpiration} " +
+                         ", with slidingExpiration : {slidingExpiration}",
+                         typeof(TInput),
+                         key,
+                         _serializer.Serialize(obj),
+                         absoluteExpiration.ToString(),
+                         slidingExpiration.ToString());
+
+        var option = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpiration = absoluteExpiration,
+            SlidingExpiration = slidingExpiration
+        };
+
+        _cache.Set(key, Encoding.UTF8.GetBytes(_serializer.Serialize(obj)), option);
+    }
+
+    public TOutput Get<TOutput>(string key)
+    {
+        var result = _cache.GetString(key);
+
+        if (!string.IsNullOrWhiteSpace(result))
+        {
+            _logger.LogTrace("DistributedCache Redis Adapter Successful Get Cache with key : {key} and data : {data}", key, _serializer.Serialize(result));
+
+            return _serializer.Deserialize<TOutput>(result);
+        }
+        else
+        {
+            _logger.LogTrace("DistributedCache Redis Adapter Failed Get Cache with key : {key}", key);
+
+            return default;
+        }
+    }
+
+    public void RemoveCache(string key)
+    {
+        _logger.LogTrace("DistributedCache Redis Adapter Remove Cache with key : {key}", key);
+
+        _cache.Remove(key);
+    }
+
+    #endregion
+
+}
