7842fbf65c1df04fa78687ca6674e77ae7096c33
Vahid <va6hid@outlook.com>
2025-01-20 12:33:52 +0330

    implement Mahta.Extensions.Translations.Parrot

---
+++ Extentsions/Translations/Mahta.Extensions.Translations.Parrot/Mahta.Extensions.Translations.Parrot/Extensions/DependencyInjection/ParrotTranslatorServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Mahta.Extensions.Translations.Abstractions;
+using Mahta.Extensions.Translations.Parrot.Options;
+using Mahta.Extensions.Translations.Parrot.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mahta.Extensions.DependencyInjection;
+
+public static class ParrotTranslatorServiceCollectionExtensions
+{
+    public static IServiceCollection AddMahtaParrotTranslator(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<ITranslator, ParrotTranslator>();
+        services.Configure<ParrotTranslatorOptions>(configuration);
+        return services;
+    }
+
+    public static IServiceCollection AddMahtaParrotTranslator(this IServiceCollection services, IConfiguration configuration, string sectionName)
+    {
+        services.AddMahtaParrotTranslator(configuration.GetSection(sectionName));
+        return services;
+    }
+
+    public static IServiceCollection AddMahtaParrotTranslator(this IServiceCollection services, Action<ParrotTranslatorOptions> setupAction)
+    {
+        services.AddSingleton<ITranslator, ParrotTranslator>();
+        services.Configure(setupAction);
+        return services;
+    }
+}
