2ef14e25352da058bf5d08abedf1f32289b4f7e3
Vahid <va6hid@outlook.com>
2025-01-20 09:51:25 +0330

    impelement Mahta.Extensions.ChangeDataLog.Sql

---
+++ Extentsions/Databases/Mahta.ExtensionsChangeDataLog/Mahta.Extensions.ChangeDataLog.Sql/Extensions/DependencyInjection/ChangeDatalogServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Mahta.Extensions.ChangeDataLog.Abstractions;
+using Mahta.Extensions.ChangeDataLog.Sql.Options;
+using Mahta.Extensions.ChangeDataLog.Sql;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mahta.Extensions.DependencyInjection;
+
+public static class changeDatalogServiceCollectionExtensions
+{
+    public static IServiceCollection AddMahtachangeDatalogDalSql(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IEntityChangeInterceptorItemRepository, DapperEntityChangeInterceptorItemRepository>();
+        services.Configure<ChangeDataLogSqlOptions>(configuration);
+        return services;
+    }
+
+    public static IServiceCollection AddMahtachangeDatalogDalSql(this IServiceCollection services, IConfiguration configuration, string sectionName)
+    {
+        services.AddMahtachangeDatalogDalSql(configuration.GetSection(sectionName));
+        return services;
+    }
+
+    public static IServiceCollection AddMahtachangeDatalogDalSql(this IServiceCollection services, Action<ChangeDataLogSqlOptions> setupAction)
+    {
+        services.AddScoped<IEntityChangeInterceptorItemRepository, DapperEntityChangeInterceptorItemRepository>();
+        services.Configure(setupAction);
+        return services;
+    }
+}
---
+++ Extentsions/Databases/Mahta.ExtensionsChangeDataLog/Mahta.Extensions.ChangeDataLog.Sql/Options/ChangeDataLogSqlOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Mahta.Extensions.ChangeDataLog.Sql.Options;
+
+public sealed record ChangeDataLogSqlOptions
+{
+    public string ConnectionString { get; set; } = string.Empty;
+    public bool AutoCreateSqlTable { get; set; } = true;
+    public string EntityTableName { get; set; } = "EntityChageDataLogs";
+    public string PropertyTableName { get; set; } = "PropertyChageDataLogs";
+    public string SchemaName { get; set; } = "dbo";
+}
