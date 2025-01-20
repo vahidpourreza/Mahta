f40e0967f149882e1fe0fc47f4af1b4ad4025b47
Vahid <va6hid@outlook.com>
2025-01-20 17:01:16 +0330

    implemenet Mahta.Extensions.Events.Outbox.Dal.EF

---
+++ Extentsions/Events/Mahta.Extensions.Events.Outbox/Mahta.Extensions.Events.Outbox.Dal.EF/BaseOutboxCommandDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Mahta.Extensions.Events.Abstractions;
+using Mahta.Extensions.Events.Outbox.Dal.EF.Configs;
+using Mahta.Extensions.Events.Outbox.Dal.EF.Interceptors;
+using Mahta.Infra.Data.Sql.Commands;
+
+namespace Mahta.Extensions.Events.Outbox.Dal.EF;
+
+public abstract class BaseOutboxCommandDbContext : BaseCommandDbContext
+{
+    public DbSet<OutBoxEventItem> OutBoxEventItems { get; set; }
+
+    public BaseOutboxCommandDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    protected BaseOutboxCommandDbContext()
+    {
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.AddInterceptors(new AddOutBoxEventItemInterceptor());
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new OutBoxEventItemConfig());
+    }
+
+
+}
