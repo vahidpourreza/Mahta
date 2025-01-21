931fab68cdfa47b6a4c0795635605aa175498282
Vahid <va6hid@outlook.com>
2025-01-21 09:35:45 +0330

    implement Mahta.Extensions.Events.PollingPublisher.Dal.Dapper

---
+++ Extentsions/Events/Mahta.Extensions.Events.PollingPublisher/Mahta.Extensions.Events.PollingPublisher.Dal.Dapper/Extensions/DependencyInjection/PollingPublisherServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Mahta.Extensions.Events.Abstractions;
+using Mahta.Extensions.Events.PollingPublisher.Dal.Dapper.DataAccess;
+using Mahta.Extensions.Events.PollingPublisher.Dal.Dapper.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mahta.Extensions.DependencyInjection;
+public static class PollingPublisherServiceCollectionExtensions
+{
+
+
+    public static IServiceCollection AddMahtaPollingPublisherDalSql(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<PollingPublisherDalRedisOptions>(configuration);
+        AddServices(services);
+        return services;
+    }
+
+    public static IServiceCollection AddMahtaPollingPublisherDalSql(this IServiceCollection services, IConfiguration configuration, string sectionName)
+    {
+        services.AddMahtaPollingPublisherDalSql(configuration.GetSection(sectionName));
+        return services;
+    }
+
+    public static IServiceCollection AddMahtaPollingPublisherDalSql(this IServiceCollection services, Action<PollingPublisherDalRedisOptions> setupAction)
+    {
+        services.Configure(setupAction);
+        AddServices(services);
+        return services;
+    }
+
+
+
+    private static void AddServices(IServiceCollection services)
+    {
+        services.AddSingleton<IOutBoxEventItemRepository, SqlOutBoxEventItemRepository>();
+    }
+
+}
---
+++ Extentsions/Events/Mahta.Extensions.Events.PollingPublisher/Mahta.Extensions.Events.PollingPublisher.Dal.Dapper/Options/PollingPublisherOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Mahta.Extensions.Events.PollingPublisher.Dal.Dapper.Options;
+public class PollingPublisherDalRedisOptions
+{
+    public string ApplicationName { get; set; }
+    public string ConnectionString { get; set; }
+    public string SelectCommand { get; set; } = "Select top (@Count) * from zamin.OutBoxEventItems where IsProcessed = 0";
+    public string UpdateCommand { get; set; } = "Update zamin.OutBoxEventItems set IsProcessed = 1 where OutBoxEventItemId in @Ids";
+}
