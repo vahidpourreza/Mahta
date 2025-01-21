f691f593aa2952bb186fab02dd1657669f6c1285
Vahid <va6hid@outlook.com>
2025-01-21 10:24:28 +0330

    implement Mahta.Extensions.MessageBus.MessageInbox.Dal.Dapper

---
+++ Extentsions/MessageBus/Mahta.Extensions.MessageBus.MessageInbox/Mahta.Extensions.MessageBus.MessageInbox.Dal.Dapper/Extensions/DependencyInjection/MessageInboxServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Mahta.Extensions.MessageBus.Abstractions;
+using Mahta.Extensions.MessageBus.MessageInbox.Dal.Dapper.Options;
+using Mahta.Extensions.MessageBus.MessageInbox.Dal.Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mahta.Extensions.DependencyInjection;
+
+public static class MessageInboxServiceCollectionExtensions
+{
+
+    public static IServiceCollection AddMahtaMessageInboxDalSql(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<MessageInboxDalDapperOptions>(configuration);
+        AddServices(services);
+        return services;
+    }
+
+    public static IServiceCollection AddMahtaMessageInboxDalSql(this IServiceCollection services, IConfiguration configuration, string sectionName)
+    {
+        services.AddMahtaMessageInboxDalSql(configuration.GetSection(sectionName));
+        return services;
+    }
+
+    public static IServiceCollection AddMahtaMessageInboxDalSql(this IServiceCollection services, Action<MessageInboxDalDapperOptions> setupAction)
+    {
+        services.Configure(setupAction);
+        AddServices(services);
+        return services;
+    }
+
+
+
+    private static void AddServices(IServiceCollection services)
+    {
+        services.AddSingleton<IMessageInboxItemRepository, SqlMessageInboxItemRepository>();
+    }
+
+
+}
---
+++ Extentsions/MessageBus/Mahta.Extensions.MessageBus.MessageInbox/Mahta.Extensions.MessageBus.MessageInbox.Dal.Dapper/Options/MessageInboxDalDapperOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Mahta.Extensions.MessageBus.MessageInbox.Dal.Dapper.Options;
+public class MessageInboxDalDapperOptions
+{
+    public string ConnectionString { get; set; } = string.Empty;
+    public bool AutoCreateSqlTable { get; set; } = true;
+    public string TableName { get; set; } = "MessageInbox";
+    public string SchemaName { get; set; } = "Mahta";
+}
