cdd2fb124624cc8906f0ee87b06c794e4e1e7ce8
Vahid <va6hid@outlook.com>
2025-01-21 08:53:53 +0330

    change the event outbox interface

--- Extentsions/Mahta.Extensions.Abstractions/Mahta.Extensions.Events.Abstractions/IOutBoxEventItemRepository.cs
+++ Extentsions/Mahta.Extensions.Abstractions/Mahta.Extensions.Events.Abstractions/IOutBoxEventItemRepository.cs
@@ -2,6 +2,6 @@
 
 public interface IOutBoxEventItemRepository
 {
-    public List<OutBoxEventItem> GetOutBoxEventItemsForPublishe(int maxCount = 100);
+    public List<OutBoxEventItem> GetOutBoxEventItemsForPublish(int maxCount = 100);
     void MarkAsRead(List<OutBoxEventItem> outBoxEventItems);
 }
