6c1aca9bb54e0e47e8058bc399f53cd7fc349812
Vahid <va6hid@outlook.com>
2025-01-22 08:57:14 +0330

    change the PollingPublisherDalRedisOptions select/update command

--- Extentsions/Events/Mahta.Extensions.Events.PollingPublisher/Mahta.Extensions.Events.PollingPublisher.Dal.Dapper/Options/PollingPublisherOptions.cs
+++ Extentsions/Events/Mahta.Extensions.Events.PollingPublisher/Mahta.Extensions.Events.PollingPublisher.Dal.Dapper/Options/PollingPublisherOptions.cs
@@ -3,6 +3,6 @@ public class PollingPublisherDalRedisOptions
 {
     public string ApplicationName { get; set; }
     public string ConnectionString { get; set; }
-    public string SelectCommand { get; set; } = "Select top (@Count) * from zamin.OutBoxEventItems where IsProcessed = 0";
-    public string UpdateCommand { get; set; } = "Update zamin.OutBoxEventItems set IsProcessed = 1 where OutBoxEventItemId in @Ids";
+    public string SelectCommand { get; set; } = "Select top (@Count) * from mahta.OutBoxEventItems where IsProcessed = 0";
+    public string UpdateCommand { get; set; } = "Update mahta.OutBoxEventItems set IsProcessed = 1 where OutBoxEventItemId in @Ids";
 }
