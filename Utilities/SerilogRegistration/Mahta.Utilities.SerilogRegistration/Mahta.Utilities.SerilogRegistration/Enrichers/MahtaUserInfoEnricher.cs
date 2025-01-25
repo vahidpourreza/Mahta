77b510f6c5c9b554789ec1489c4cef336013b85b
Vahid <va6hid@outlook.com>
2025-01-25 09:22:07 +0330

    implement Mahta.Utilities.SerilogRegistration

---
+++ Utilities/SerilogRegistration/Mahta.Utilities.SerilogRegistration/Mahta.Utilities.SerilogRegistration/Enrichers/MahtaUserInfoEnricher.cs
@@ -0,0 +1,41 @@
+﻿using Mahta.Extensions.UsersManagement.Abstractions;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Mahta.Utilities.SerilogRegistration.Enrichers;
+public class MahtaUserInfoEnricher : ILogEventEnricher
+{
+    private readonly IUserInfoService _userInfoService;
+
+    public MahtaUserInfoEnricher(IUserInfoService userInfoService)
+    {
+        this._userInfoService = userInfoService;
+    }
+
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory factory)
+    {
+        string userName;
+        string UserId;
+        string UserIp;
+        string clientId;
+
+        userName = _userInfoService.GetUsername();
+        if (string.IsNullOrEmpty(userName))
+            userName = "Unknown";
+        UserId = _userInfoService.UserIdOrDefault();
+        UserIp = _userInfoService.GetUserIp();
+        clientId = _userInfoService.GetClaim("client_id");
+        if (string.IsNullOrEmpty(clientId))
+            clientId = "Unknown";
+
+        var userNameProperty = factory.CreateProperty("UserName", userName);
+        var userIdProperty = factory.CreateProperty("UserId", UserId);
+        var userIpProperty = factory.CreateProperty("UserIp", UserIp);
+        var clientIdProperty = factory.CreateProperty("ClientId", clientId);
+
+        logEvent.AddPropertyIfAbsent(userNameProperty);
+        logEvent.AddPropertyIfAbsent(userIdProperty);
+        logEvent.AddPropertyIfAbsent(userIpProperty);
+        logEvent.AddPropertyIfAbsent(clientIdProperty);
+    }
+}
---
+++ Utilities/SerilogRegistration/Mahta.Utilities.SerilogRegistration/Mahta.Utilities.SerilogRegistration/Extensions/DependencyInjection/SerilogServiceCollectionExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Mahta.Utilities.SerilogRegistration.Enrichers;
+using Mahta.Utilities.SerilogRegistration.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Core;
+using Serilog.Enrichers.Span;
+using Serilog.Exceptions;
+
+namespace Mahta.Extensions.DependencyInjection;
+public static class SerilogServiceCollectionExtensions
+{
+    public static WebApplicationBuilder AddMahtaSerilog(this WebApplicationBuilder builder, IConfiguration configuration, params Type[] enrichersType)
+    {
+
+        builder.Services.Configure<SerilogApplicationEnricherOptions>(configuration);
+        return AddServices(builder, enrichersType);
+    }
+
+    public static WebApplicationBuilder AddMahtaSerilog(this WebApplicationBuilder builder, IConfiguration configuration, string sectionName, params Type[] enrichersType)
+    {
+        return builder.AddMahtaSerilog(configuration.GetSection(sectionName), enrichersType);
+    }
+
+    public static WebApplicationBuilder AddMahtaSerilog(this WebApplicationBuilder builder, Action<SerilogApplicationEnricherOptions> setupAction, params Type[] enrichersType)
+    {
+        builder.Services.Configure(setupAction);
+        return AddServices(builder, enrichersType);
+    }
+
+    private static WebApplicationBuilder AddServices(WebApplicationBuilder builder, params Type[] enrichersType)
+    {
+
+        List<ILogEventEnricher> logEventEnrichers = new();
+
+        builder.Services.AddTransient<MahtaUserInfoEnricher>();
+        builder.Services.AddTransient<MahtaApplicaitonEnricher>();
+        foreach (var enricherType in enrichersType)
+        {
+            builder.Services.AddTransient(enricherType);
+        }
+
+        builder.Host.UseSerilog((ctx, services, lc) => {
+            logEventEnrichers.Add(services.GetRequiredService<MahtaUserInfoEnricher>());
+            logEventEnrichers.Add(services.GetRequiredService<MahtaApplicaitonEnricher>());
+            foreach (var enricherType in enrichersType)
+            {
+                logEventEnrichers.Add(services.GetRequiredService(enricherType) as ILogEventEnricher);
+            }
+
+            lc
+            //.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+            .Enrich.FromLogContext()
+            .Enrich.With([.. logEventEnrichers])
+            .Enrich.WithExceptionDetails()
+            .Enrich.WithSpan()
+            .ReadFrom.Configuration(ctx.Configuration);
+        });
+        return builder;
+    }
+}
